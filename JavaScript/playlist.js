ef5db96275250064ef2dae859e43aadcb9cdec39
Marin_McCormack <maringotgame@icloud.com>
2023-12-14 11:09:14 -0500

    Add playlist

--- JavaScript/playlist.js
+++ JavaScript/playlist.js
@@ -1,8 +1,14 @@
 window.onload = init;
 function init() {
- var button = document.getElementById("addButton");
+ let button = document.getElementById("addButton");
  button.onclick = handleButtonClick;
 }
 function handleButtonClick() {
- alert("Button was clicked!");
+    let textInput = document.getElementById("songTextInput");
+    let songName = textInput.value;
+
+let li = document.createElement("li");
+li.innerHTML = songName;
+let ul = document.getElementById("playlist");
+ul.appendChild(li);
 }
