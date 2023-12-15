a5b4e679b74fdb4bce307103c0d718cce0f72a58
Marin_McCormack <maringotgame@icloud.com>
2023-12-15 10:16:37 -0500

    add indentation

--- JavaScript/playlist.js
+++ JavaScript/playlist.js
@@ -7,9 +7,9 @@ function handleButtonClick() {
     let textInput = document.getElementById("songTextInput");
     let songName = textInput.value;
 
-let li = document.createElement("li");
-li.innerHTML = songName;
-let ul = document.getElementById("playlist");
-ul.appendChild(li);
-save(songName);
+    let li = document.createElement("li");
+    li.innerHTML = songName;
+    let ul = document.getElementById("playlist");
+    ul.appendChild(li);
+    save(songName);
 }
