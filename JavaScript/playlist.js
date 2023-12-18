63c7a681e0f7bb65473debef00f85bbec1810a27
Marin_McCormack <maringotgame@icloud.com>
2023-12-18 11:11:05 -0500

    rmeove text

--- JavaScript/playlist.js
+++ JavaScript/playlist.js
@@ -1,9 +1,3 @@
-window.onload = init;
-function init() {
-    let button = document.getElementById("addButton");
-    button.onclick = handleButtonClick;
-    loadPlaylist();
-}
 function handleButtonClick() {
     let textInput = document.getElementById("songTextInput");
     let songName = textInput.value;
